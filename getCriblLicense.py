cdf608cc9f1752322b5f40ad3769bd1085faeb21
Ryan Hennessy <ryan.hennessy@me.com>
2024-12-17 08:18:55 -0600

    Ran everything through formatter

--- getCriblLicense.py
+++ getCriblLicense.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 import requests
-import getpass 
+import getpass
 import datetime
 
-loginData={}
+loginData = {}
+criblHeaders = {"Content-type": "application/json", "Accept": "application/json"}
+criblAuthUrl = criblUrl + ":9000/api/v1/auth/login"
+criblLicUrl = criblUrl + ":9000/api/v1/system/licenses/usage"
 
-#Uncomment and set the following values for the url, login, and password 
+# Uncomment and set the following values for the url, login, and password
 # to hard codd them. The script will then not probmpt for the values via stdin
 ############################################################################
 # criblUrl = "http://criblleader.com"
@@ -14,43 +17,37 @@ loginData={}
 # loginData['password'] = "Mypassword1"
 #############################################################################
 
-criblHeaders={'Content-type': 'application/json', 'Accept': 'application/json'}
-
-
-if 'criblUrl' not in vars() or loginData == {}:
+if "criblUrl" not in vars() or loginData == {}:
     criblUrl = input("Cribl Leaader URL (https://leader.examle.com): ").rstrip()
-    loginData['username'] = input("Login: ").rstrip()
-    loginData['password'] = getpass.getpass("Password: ")
+    loginData["username"] = input("Login: ").rstrip()
+    loginData["password"] = getpass.getpass("Password: ")
 
-criblAuthUrl = criblUrl + ":9000/api/v1/auth/login"
-criblLicUrl = criblUrl + ":9000/api/v1/system/licenses/usage"
 
 try:
     resp = requests.post(criblAuthUrl, json=loginData, headers=criblHeaders)
-    criblToken = resp.json()['token']
-
+    criblToken = resp.json()["token"]
 except Exception:
     print("\nLogin Failed")
     print("------------------------------------")
     print(str(resp.status_code) + " " + resp.text)
     exit(1)
 
-criblHeaders['Authorization'] = "Bearer " + criblToken
+criblHeaders["Authorization"] = "Bearer " + criblToken
 
-resp=requests.get(criblLicUrl, headers=criblHeaders)
-licData=resp.json()
-
-
-with open('usage.csv', 'w') as csvfile:
-    csvfile.write('Date, Gigabytes In, Gigabytes Out, Events In, Events Out')
-    line="\n"
-    for i in range(licData['count']):
-       line+=datetime.datetime.fromtimestamp((licData['items'][i]['startTime']/1000)+21600).strftime('%m-%d-%y')
-       line+=f",{str(float(licData['items'][i]['inBytes'])/1000000000)}"                                                                           
-       line+=f",{str(float(licData['items'][i]['outBytes'])/1000000000)}"                                                                           
-       line+=f",{licData['items'][i]['inEvents']}"                                                                           
-       line+=f",{licData['items'][i]['outEvents']}"      
-       csvfile.write(line)
-       line="\n"
+resp = requests.get(criblLicUrl, headers=criblHeaders)
+licData = resp.json()
 
 
+with open("usage.csv", "w") as csvfile:
+    csvfile.write("Date, Gigabytes In, Gigabytes Out, Events In, Events Out")
+    line = "\n"
+    for i in range(licData["count"]):
+        line += datetime.datetime.fromtimestamp(
+            (licData["items"][i]["startTime"] / 1000) + 21600
+        ).strftime("%m-%d-%y")
+        line += f",{str(float(licData['items'][i]['inBytes'])/1000000000)}"
+        line += f",{str(float(licData['items'][i]['outBytes'])/1000000000)}"
+        line += f",{licData['items'][i]['inEvents']}"
+        line += f",{licData['items'][i]['outEvents']}"
+        csvfile.write(line)
+        line = "\n"
